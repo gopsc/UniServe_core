3f457c6d48e9f58805ef31e517a33db7eefbec2b
qing <1776177662@qq.com>
2026-05-10 00:53:08 +0800

    整理代码

--- include/nn/NNBuilder.hpp
+++ include/nn/NNBuilder.hpp
@@ -3,56 +3,19 @@
 #include <stdexcept>
 #include "nn/NeuralNetwork.hpp"
 namespace qing {
+/*  TODO: 增加将一层非线性层分解为两层的算法 */
+
 class NNBuilder {  /* 神经网络构建器 */
 public:
-
-	void add(NeuralNetwork &layer);
-
+	void add(NeuralNetwork &layer);	/* 向神经网络中添加全连接层 */
 	std::vector<std::pair<long, long>> get_shape() const;	/* 获取变换层的形状 */
-
 	void print_shape() const;	/* 向标准输出打印形状 */
-
 	void save(std::ostream& out);	/* 向输出流储存模型 */
-
-    /* 从输入流加载模型 */
-    void load(std::istream& in) {
-        nn.clear();
-        while(true) {
-            try{
-                auto layer = NeuralNetwork::Load_in_Factory(in);
-                add(layer);
-            }
-	    catch (std::runtime_error& e) { break; }
-	}
-    }
-
-    /*
-     * 将一层非线性层分裂为两层，相当于插入一层神经元
-     */
-    //void fork(int index, long num, float rate) {
-
-    /*
-     * 神经网络生长，增加第layer层，每层增加num个神经元
-     *
-     * layer: 增加的层数
-     * num: 增加的神经元数量
-     *
-     * FIXME: 神经网络生长时应当冻结其它层（除了最后一层？）（learning_rate == 0）
-     */
-    //void grow(int layer, int num) {
-
-    /* 前向反馈 */
-    std::vector<float> forward(std::vector<float>& r);
-
-    /* 反向传播 */
-    std::vector<float> backward(std::vector<float>& errs, float discount = 1.0);
-
-    /* 计算误差 */
-    std::vector<float> cal_err(std::vector<float>& r, std::vector<float>& t);
-
-
-
+	void load(std::istream& in);	/* 从输入流加载模型 */
+	std::vector<float> forward(std::vector<float>& r);	/* 前向反馈 */
+	std::vector<float> backward(std::vector<float>& errs, float discount = 1.0);	/* 反向传播 */
+	std::vector<float> cal_err(std::vector<float>& r, std::vector<float>& t);	/* 计算误差 */
 private:
-    std::vector<NeuralNetwork> nn;
+	std::vector<NeuralNetwork> nn;
 };
 }
